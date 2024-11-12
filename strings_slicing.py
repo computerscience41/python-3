157e9c6d1afe9f73d15c338d0289f0fb24f77771
computerscience41 <rakibhasan934541@gmail.com>
2024-11-12 15:11:04 +0600

    strings slicing

---
+++ strings_slicing.py
@@ -0,0 +1,10 @@
+# 4 String slicing
+
+sentence = " Learning python id fun!"
+
+word_python = sentence[9:15]
+word_fun = sentence[-4:-1]
+
+print("Extracted word:", word_python)
+print("Extracted word:", word_fun)
+
