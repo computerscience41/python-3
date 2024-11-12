4d66e05a07cd4d03b8e90aac9cee6d88f3e59522
computerscience41 <rakibhasan934541@gmail.com>
2024-11-12 15:53:55 +0600

    set operation

---
+++ set-operation.py
@@ -0,0 +1,10 @@
+# 10 Set operations
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 5, 6, 7, 8}
+
+intersection = set1.intersection(set2)
+print("Intersection:", intersection)
+
+union = set1.union(set2)
+print("union:", union)
