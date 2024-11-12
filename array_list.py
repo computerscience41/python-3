9460af12890e5691e487bb1579e25fbcf4046547
computerscience41 <rakibhasan934541@gmail.com>
2024-11-12 15:42:29 +0600

    array list

---
+++ array_list.py
@@ -0,0 +1,12 @@
+# 7 List Creation and Access
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+print("First fruit:", fruits[0])
+print("last fruit:", fruits[-1])
+
+fruits.append("fig")
+print("Updated list:", fruits)
+
+fruits.pop(1)
+print("List after removal:", fruits)
