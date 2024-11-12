2d1a6a290405ed42c285f3cb89b4c540364b9d0a
computerscience41 <rakibhasan934541@gmail.com>
2024-11-12 16:02:27 +0600

    list slicing

---
+++ listing-slicing.py
@@ -0,0 +1,9 @@
+# 13 listing slicing
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print("First 3 elements:", numbers[:3])
+
+print("Last 3 elements:", numbers[-3:])
+
+print("Elements from index 2 to 6:", numbers[2:7])
