9eeac1311e2ab87ed023d9598813d645c9767ff4
computerscience41 <rakibhasan934541@gmail.com>
2024-11-12 15:56:43 +0600

    direction

---
+++ dicrection.py
@@ -0,0 +1,13 @@
+# 11 dictionary creation and access
+
+person = {
+    "name": "john",
+    "age": "30",
+    "city": "New York"
+}
+
+print("Name:", person["name"])
+
+person["profession"] = "Engineer"
+print("Updated dictionary:", person)
+
