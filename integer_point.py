ec9c78cc6977578284ffe69dc27eff90b231dc96
computerscience41 <rakibhasan934541@gmail.com>
2024-11-12 14:46:00 +0600

    integer

---
+++ integer_point.py
@@ -0,0 +1,15 @@
+# Integer operations
+a = 10
+b =  5
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
