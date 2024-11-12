943db6d99c314250c8a77eaded4c67f421168d75
computerscience41 <rakibhasan934541@gmail.com>
2024-11-12 15:06:33 +0600

    strings

---
+++ strings.py
@@ -0,0 +1,8 @@
+# String Concatenation
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+
+greeting = "Hello "  + first_name + " " + last_name + ", welcome!"
+
+print(greeting)
