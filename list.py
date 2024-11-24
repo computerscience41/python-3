7782f57cb74dada6bd50dec604652a29ce957080
computerscience41 <rakibhasan934541@gmail.com>
2024-11-24 15:19:15 +0600

    list

---
+++ list.py
@@ -0,0 +1,15 @@
+# List creation and access
+
+fruits = ["apple","banana","orange","mango","cherry"]
+
+# Access the first and last fruits
+print("first fruits:", fruits[0])
+print("last fruits:", fruits[-1])
+
+# add a new fruits
+fruits.append("date")
+print("new fruits:",fruits)
+
+# Remove the second fruits
+fruits.pop(1)
+print("Remove:", fruits)
