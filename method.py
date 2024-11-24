e4b0ad284e19cd7f1e94b891b915bd432059584a
computerscience41 <rakibhasan934541@gmail.com>
2024-11-24 15:01:45 +0600

    method

---
+++ method.py
@@ -0,0 +1,9 @@
+# String method
+
+text = "Data science is amazing"
+
+uppercase_text = text.upper()
+replaced_text = text.replace("amazing", "challenging")
+
+print(uppercase_text)
+print(replaced_text)
