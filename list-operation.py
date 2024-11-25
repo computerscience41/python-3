b640ebd53b2879b9ae2c339a753042bca33aab2c
computerscience41 <rakibhasan934541@gmail.com>
2024-11-25 13:01:47 +0600

    list operation

---
+++ list-operation.py
@@ -0,0 +1,11 @@
+# List number
+
+numbers = [2,4,6,8,10]
+
+total_sum = sum(numbers)
+print("Sum of all numbers:", total_sum)
+
+# multiply number by 2
+
+new_numbers = [num * 2 for num in numbers]
+print("number multiply by 2:", new_numbers)
