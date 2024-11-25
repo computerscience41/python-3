ab7adf5f0fb486f4e682dffc533cba5e51d94631
computerscience41 <rakibhasan934541@gmail.com>
2024-11-25 13:18:15 +0600

    tuple contain

---
+++ tuple.py
@@ -0,0 +1,9 @@
+# Tuple creation and access
+
+my_tuple = ("python", 42, 3.14, True)
+
+print("Third element:",my_tuple[2])
+
+# check if the tuple contains true
+contains_true = True in my_tuple
+print("Contains True:", contains_true)
