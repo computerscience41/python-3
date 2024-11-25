ee3e83db46d39039b1605624cef81312e05d4b18
computerscience41 <rakibhasan934541@gmail.com>
2024-11-25 13:45:18 +0600

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+# Dictionary creation and access
+
+person = {
+    "name" : "John",
+    "age" : 25,
+    "city" : "new york"
+}
+
+print("Name:", person["name"])
+
+# print add profession
+person["profession"] = "Engineer"
+print("Profession:",person)
